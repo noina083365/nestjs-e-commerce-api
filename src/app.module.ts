6d370263ac1a0b888bc77ae36a5af907b7753411
Noi.Skuberg <kanokpit.bluphoenix@gmail.com>
2024-12-03 06:20:44 +0700

    feat: add main resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -21,8 +26,13 @@ import { AppService } from './app.service';
 			}),
 			inject: [ConfigService], // Inject ConfigService to use it in useFactory
 		}),
+		UsersModule,
+		CustomersModule,
+		CategoriesModule,
+		ProductsModule,
+		OrdersModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
