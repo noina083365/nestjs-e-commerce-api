5140c6b8704f751b458d71bf7691075c765d0ac8
Noi.Skuberg <kanokpit.bluphoenix@gmail.com>
2024-12-03 06:03:44 +0700

    fix: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule], // Make ConfigModule available
+			useFactory: async (configService: ConfigService) => ({
+				type: 'mysql',
+				host: configService.get<string>('DB_HOST'),
+				port: parseInt(configService.get<string>('DB_PORT')),
+				username: configService.get<string>('DB_USERNAME'),
+				password: configService.get<string>('DB_PASSWORD'),
+				database: configService.get<string>('DB_DATABASE'),
+				entities: ['dist/**/*.entity{.ts,.js}'],
+				synchronize: true,
+			}),
+			inject: [ConfigService], // Inject ConfigService to use it in useFactory
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
