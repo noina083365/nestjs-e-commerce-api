373f1b6a7e6959179ea5caa49643ad0d4611a209
Noi.Skuberg <kanokpit.bluphoenix@gmail.com>
2024-12-04 07:57:28 +0700

    fix: add dto

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,57 @@
-export class CreateCustomerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+import {
+	IsEmail,
+	IsOptional,
+	Length,
+	Matches,
+} from 'class-validator';
+import { ActiveStatus } from 'src/common/interfaces';
+
+export class CreateCustomerDto {
+	@ApiProperty({ example: 'user_ecom', description: 'Username of the user' })
+	@Length(4, 20)
+	@Matches(/^[a-zA-Z0-9_]*$/, {
+		message: 'Username can only contain letters, numbers, and underscores',
+	})
+	username: string;
+
+	@ApiProperty({ example: 'First', description: 'First name' })
+	@Length(1, 50)
+	@Matches(/^[a-zA-Z0-9- ]*$/, {
+		message: 'Firstname can only contain letters, numbers, hyphens, spaces',
+	})
+	firstName: string;
+
+	@ApiProperty({ example: 'Last', description: 'Last name' })
+	@Length(1, 50)
+	@Matches(/^[a-zA-Z0-9- ]*$/, {
+		message: 'Lastname can only contain letters, numbers, hyphens, spaces',
+	})
+	lastName: string;
+
+	@ApiProperty({ example: 'UserTest@test.com', description: 'email of the user' })
+	@IsEmail()
+	email: string;
+
+	@ApiProperty({
+		example: 'House no. 777 Moo 7 T. Chong Maew, A. Muang, Nakhon Ratchasima 30270 THAILAND',
+		description: 'Last name'
+	})
+	@IsOptional()
+	address: string;
+
+	@ApiProperty({ example: 'ECom001!', description: 'Password' })
+	password: string;
+
+	@ApiProperty({ enum: ['active', 'inactive'] })
+	status: ActiveStatus;
+
+	@ApiProperty({ description: 'Created date' })
+	@IsOptional()
+	createdAt?: Date;
+
+	@ApiProperty({ description: 'Updated date' })
+	@IsOptional()
+	updatedAt?: Date;
+}
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,44 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  Length,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @ApiProperty({
+    example: 'Keyboard',
+    description: 'Name of the product',
+  })
+  @Length(3, 125)
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    example: 'The easiest keyboard to use for coding.',
+    description: 'Description of the product',
+  })
+  @Length(3, 200)
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty({
+    example: '359',
+    description: 'Price of the product',
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @ApiProperty({
+    example: '10',
+    description: 'Stock of the product',
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  stock: number;
+}
