8b573751fb87dc4b4dd5cf71547164cfef4d3644
Noi.Skuberg <kanokpit.bluphoenix@gmail.com>
2024-12-05 22:49:58 +0700

    feat: product list

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
+	constructor(
+		@InjectRepository(Product)
+		private readonly productRepository: Repository<Product>,
+	) { }
+
 	create(createProductDto: CreateProductDto) {
 		return 'This action adds a new product';
 	}
 
 	findAll() {
-		return `This action returns all products`;
+		try {
+			return this.productRepository.find();
+		} catch (error) {
+			throw new HttpException(error.message || 'Internal Server Error.', error.status || 500);
+		}
 	}
 
 	findOne(id: number) {
