7e762d8feffb779ca841a5bf30dc8e90732eadda
Noi.Skuberg <kanokpit.bluphoenix@gmail.com>
2024-12-09 01:07:52 +0700

    fix: username for customer login support email format

--- src/auth/dto/customer-login.dto.ts
+++ src/auth/dto/customer-login.dto.ts
@@ -8,7 +8,7 @@ import {
 
 export class CustomerLoginDto {
   @ApiProperty({ example: 'uswshopping', description: 'Username' })
-  @Length(4, 20)
+  @Length(7, 50)
   @Matches(/^[a-zA-Z0-9_@.]*$/, {
     message: 'Username can only contain letters, numbers, and underscores',
   })
