fe0392f813570f9b068c00c62fa3820679116634
Kanokpit <kanokpit.y@gmail.com>
2024-12-18 19:56:10 +0700

    fix: buy now api

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
 	constructor(private readonly ordersService: OrdersService) {}
 
 	@Post()
-	create(@Body() createOrderDto: CreateOrderDto) {
-		return this.ordersService.create(createOrderDto);
+	create(@Body() orderData: any) {
+		// Buy Now
+		return this.ordersService.createOrder(orderData);
+
+		// Checkout
+		// return this.ordersService.createOrderFromCart(orderData);
 	}
 
 	@Get()
@@ -24,7 +27,7 @@ export class OrdersController {
 
 	@Patch(':id')
 	update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-		return this.ordersService.update(+id, updateOrderDto);
+		return this.ordersService.updateOrder(+id, updateOrderDto.status);
 	}
 
 	@Delete(':id')
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,10 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
-import { Product } from '../products/entities/product.entity';
 import { OrderItem } from '../order-item/entities/order-item.entity';
 
 @Injectable()
@@ -14,52 +12,138 @@ export class OrdersService {
 		private readonly orderRepository: Repository<Order>,
 		@InjectRepository(OrderItem)
 		private readonly orderItemRepository: Repository<OrderItem>,
-		@InjectRepository(Product)
-		private readonly productRepository: Repository<Product>,
 	) { }
 
-	// TODO: Remove later
-	create(createOrderDto: CreateOrderDto) {
-		return `This action returns a new order`;
+	findOne(id: number) {
+		try {
+			return this.orderRepository.findOneBy({ id });
+		} catch (error) {
+			throw new HttpException(error.message || 'Internal Server Error.', error.status || 500);
+		}
 	}
 
-	async addProductToOrder(orderId: number, productId: number): Promise<OrderItem> {
-		const order = await this.orderRepository.findOne({
-			where: { id: orderId },
-			relations: ['products'],
-		});
-		const product = await this.productRepository.findOne({
-			where: { id: productId }
-		});
+	async createOrder(createOrderDto: CreateOrderDto) {
+		const { items, total_price, customerId } = createOrderDto;
+		try {
+			const order = await this.orderRepository.save({
+				customer: { id: customerId },
+				status: 'Pending',
+				total_price
+			});
 
-		if (!order || !product) {
-			throw new Error('Order or Product not found');
+			if (order && order.id) {
+				items.map(async (item: any) => {
+					const orderItem = {
+						order: { id: order.id },
+						price: parseFloat(item.price),
+						product: { id: parseInt(item.id) },
+						quantity: parseInt(item.quantity)
+					};
+					await this.orderItemRepository.save(orderItem);
+				});
+			} else {
+				throw new HttpException('Cannot update cart.', 500);
+			}
+		} catch (error) {
+			throw new HttpException(error.message || 'Internal Server Error.', error.status || 500);
 		}
+	}
 
-		const item = {
-			order,
-			product,
-			price: product.price,
-			quantity: 1
+	async createOrderFromCart(checkoutData: any) { // Cart
+		const { cartId } = checkoutData;
+		try {
+			const cart = {};
+			/*
+			const order = await this.orderRepository.save({
+				customer: { id: customerId },
+				status: 'Pending',
+				total_price
+			});
+
+			if (order && order.id) {
+				items.map(async (item: any) => {
+					const orderItem = {
+						order: { id: order.id },
+						price: parseFloat(item.price),
+						product: { id: parseInt(item.id) },
+						quantity: parseInt(item.quantity)
+					};
+					await this.orderItemRepository.save(orderItem);
+				});
+				// update cart to Checkout
+			} else {
+				throw new HttpException('Cannot update cart.', 500);
+			}
+			*/
+		} catch (error) {
+			throw new HttpException(error.message || 'Internal Server Error.', error.status || 500);
 		}
-
-		const orderItem = this.orderItemRepository.create(item);
-		return orderItem;
 	}
 
 	findAll() {
-		return `This action returns all orders`;
+		try {
+			return this.orderRepository.find();
+		} catch (error) {
+			throw new HttpException(error.message || 'Internal Server Error.', error.status || 500);
+		}
 	}
 
-	findOne(id: number) {
-		return `This action returns a #${id} order`;
+	async orderList(customerId: number, status: string = 'Pending') {
+		let orderList: any[] = [];
+		try {
+			const orders = await this.orderRepository.find({
+				where: {
+					customer: { id: customerId },
+					status
+				},
+			});
+			if (orders && orders.length) {
+				// 	const cartItems = await this.orderRepository.find({
+				// 		where: {
+				// 			order: { id: cartOpen.id }
+				// 		},
+				// 		relations: ['product']
+				// 	});
+				// 	orderList = cartItems.map((cItem: CartItem) => {
+				// 		const newItemKey = {
+				// 			id: cItem.product.id,
+				// 			name: cItem.product.name,
+				// 			description: cItem.product.description,
+				// 			price: cItem.product.price,
+				// 			stock: cItem.product.stock,
+				// 			quantity: cItem.quantity,
+				// 		}
+				// 		return newItemKey;
+				// 	});
+			}
+			return orderList;
+		} catch (error) {
+			throw new HttpException(error.message || 'Internal Server Error.', error.status || 500);
+		}
 	}
 
-	update(id: number, updateOrderDto: UpdateOrderDto) {
-		return `This action updates a #${id} order`;
+	async updateOrder(id: number, status: string) {
+		let orderUpdate = null;
+		try {
+			const order = await this.orderRepository.findOne({
+				where: { id }
+			});
+			if (order) {
+				orderUpdate = await this.orderRepository.update(id, {
+					status
+				});
+			}
+			return orderUpdate;
+		} catch (error) {
+			throw new HttpException(error.message || 'Internal Server Error.', error.status || 500);
+		}
 	}
 
-	remove(id: number) {
-		return `This action removes a #${id} order`;
+	async remove(id: number) {
+		try {
+			await this.orderRepository.delete(id);
+		} catch (error) {
+			throw new HttpException(error.message || 'Internal Server Error.', error.status || 500);
+		}
 	}
 }
